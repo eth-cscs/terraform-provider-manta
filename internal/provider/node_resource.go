0be722886bae6913d62aa0d9c911f36b9dbe7ca1
Theo <grivel.theo@protonmail.com>
2025-08-29 16:42:49 +0200

    feat(provider/node): implement update function
    The update function was missing, nothing
    happen after updating the state of the node
    
    Error: Provider produced inconsistent
    result after apply
    
    When applying changes to manta_node.node,
    provider
    "provider[\"registry.terraform.io/manta\"]"
    produced an unexpected new value: .state:
    was cty.StringVal("Off"), but now
    cty.StringVal("On").
    
    This is a bug in the provider, which should
    be reported in the provider's own issue
    tracker.

--- internal/provider/node_resource.go
+++ internal/provider/node_resource.go
@@ -208,6 +208,49 @@ func (r *nodeResource) Read(ctx context.Context, req resource.ReadRequest, resp 
 
 // Update updates the resource and sets the updated Terraform state on success.
 func (r *nodeResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan nodeResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state := string(plan.State.ValueString())
+
+	if state != "" {
+		_, err := r.client.PowerNodeId(string(plan.ID.ValueString()), state)
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Error cannot swith the power status of the node: ", err.Error(),
+			)
+			return
+		}
+	}
+
+	node, err := r.client.GetNodeId(string(plan.ID.ValueString()))
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating rfe",
+			"Could not create rfe, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	plan.Type = types.StringValue(node.Type)
+	plan.State = types.StringValue(node.State)
+	plan.Flag = types.StringValue(node.Flag)
+	plan.Enabled = types.BoolValue(node.Enabled)
+	plan.Role = types.StringValue(node.Role)
+	plan.NID = types.Int64Value(int64(node.NID))
+	plan.NetType = types.StringValue(node.NetType)
+	plan.Arch = types.StringValue(node.Arch)
+	plan.Class = types.StringValue(node.Class)
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
 // Delete deletes the resource and removes the Terraform state on success.
