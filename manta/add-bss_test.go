f99f4c1b0a0d2dbc2ec683b47fd19a1605630fb0
Theo <grivel.theo@protonmail.com>
2025-10-08 10:39:01 +0200

    chore(manta/bss): refactor test

--- manta/add-bss_test.go
+++ manta/add-bss_test.go
@@ -5,20 +5,10 @@ import (
 	"testing"
 )
 
-func testAddBssSuccess(t *testing.T, added, expected BssParams) {
+func testAddBss(t *testing.T, added, expected BssParams) {
 	var w = Wrapper{Access_token: "~/access_token", Base_url: "http://localhost:3000"}
 
 	added, err := w.AddBss(added)
-	if err != nil {
-		t.Errorf(`error: %s`, err)
-	}
-
-	_, err = w.GetBssParams()
-	if err != nil {
-		t.Errorf(`error: %s`, err)
-	}
-
-	_, err = w.DeleteBss(added)
 	if err != nil {
 		t.Errorf(`error: %s`, err)
 	}
@@ -28,6 +18,47 @@ func testAddBssSuccess(t *testing.T, added, expected BssParams) {
 		fmt.Println(expected)
 		t.Errorf(`error: the added is not equal to the expected`)
 	}
+}
+
+func testDeleteBss(t *testing.T, added BssParams) {
+	var w = Wrapper{Access_token: "~/access_token", Base_url: "http://localhost:3000"}
+
+	body, err := w.DeleteBss(added)
+
+	if body != "" {
+		t.Errorf(`error: body should be empty: %s`, body)
+	}
+
+	if err != nil {
+		t.Errorf(`error: %s`, err)
+	}
+}
+
+func testGetBss(t *testing.T, expected BssParams) {
+	var w = Wrapper{Access_token: "~/access_token", Base_url: "http://localhost:3000"}
+
+	params, err := w.GetBssParams()
+
+	if err != nil {
+		t.Errorf(`error: %s`, err)
+	}
+
+	if len(params) != 1 {
+		t.Errorf(`error: boot parameters' length is not equal to 1`)
+		return
+	}
+
+	if params[0].Cmp(&expected) {
+		fmt.Println(params)
+		fmt.Println(expected)
+		t.Errorf(`error: the added is not equal to the expected`)
+	}
+}
+
+func testAddGetDelete(t *testing.T, added, expected BssParams) {
+	testAddBss(t, added, expected)
+	testGetBss(t, expected)
+	testDeleteBss(t, added)
 }
 
 func TestAddBssSuccessMinimun(t *testing.T) {
@@ -43,7 +74,7 @@ func TestAddBssSuccessMinimun(t *testing.T) {
 		Initrd: "https://example.com/initrd",
 	}
 
-	testAddBssSuccess(t, added, expected)
+	testAddGetDelete(t, added, expected)
 }
 
 func TestAddBssSuccessParams(t *testing.T) {
@@ -61,7 +92,7 @@ func TestAddBssSuccessParams(t *testing.T) {
 		Params: "console=ttyS0,115200 console=tty0",
 	}
 
-	testAddBssSuccess(t, added, expected)
+	testAddGetDelete(t, added, expected)
 }
 
 func TestAddBssSuccessTwoMacs(t *testing.T) {
@@ -79,16 +110,17 @@ func TestAddBssSuccessTwoMacs(t *testing.T) {
 		Params: "console=ttyS0,115200 console=tty0",
 	}
 
-	testAddBssSuccess(t, added, expected)
+	testAddGetDelete(t, added, expected)
 }
 
 func TestDeleteBss(t *testing.T) {
-	var w = Wrapper{Access_token: "~/access_token", Base_url: "http://localhost:3000"}
 	var added = BssParams{
-		Macs: []string{"00:de:ad:be:ef:00"},
+		Macs:   []string{"00:de:ad:be:ef:02", "00:de:ad:be:ef:03"},
+		Kernel: "https://example.com/kernel",
+		Initrd: "https://example.com/initrd",
+		Params: "console=ttyS0,115200 console=tty0",
 	}
-	a, err := w.DeleteBss(added)
 
-	fmt.Println(a)
-	fmt.Println(err)
+	testAddBss(t, added, added)
+	testDeleteBss(t, added)
 }
