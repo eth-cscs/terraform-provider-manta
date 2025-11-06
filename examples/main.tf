349cb1bc1f413bfedaed0864ca313045f035f4d1
Theo <grivel.theo@protonmail.com>
2025-11-06 11:17:26 +0100

    chore: split provider in an extra file

--- examples/main.tf
+++ examples/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    manta = {
-      source  = "registry.terraform.io/eth-cscs/manta" # to be confirmed
-      version = ">= 0.0.1"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
 provider "manta" {
   base_url     = var.base_url
   access_token = var.access_token
